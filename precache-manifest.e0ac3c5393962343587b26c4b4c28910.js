efac231b3209b67466d6d6ca6e233ddfe5d8a378
MarkMuhhin <tennnex@hotmail.com>
2019-03-09 13:35:11 +0200

    Updates

---
+++ precache-manifest.e0ac3c5393962343587b26c4b4c28910.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9534cb56e02b7c306e3d",
+    "url": "/tryingagain/static/js/runtime~main.4992dbcd.js"
+  },
+  {
+    "revision": "86f39933c86b66d4a98b",
+    "url": "/tryingagain/static/js/main.4abd6c8b.chunk.js"
+  },
+  {
+    "revision": "ac4b42af74480934667e",
+    "url": "/tryingagain/static/js/2.bfb8aca3.chunk.js"
+  },
+  {
+    "revision": "86f39933c86b66d4a98b",
+    "url": "/tryingagain/static/css/main.2a9dbd14.chunk.css"
+  },
+  {
+    "revision": "155147b89c34f7db30b258d18442b625",
+    "url": "/tryingagain/index.html"
+  }
+];
